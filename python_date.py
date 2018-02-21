e41eb4bd33c6d87c1d338bc59218d856a97c65a9
risa <risafangs@users.noreply.github.com>
2018-02-20 20:52:37 -0600

    removed unnecessary int()

--- python_date.py
+++ python_date.py
@@ -6,7 +6,7 @@ current_age = int(input('How old are you, stranger? ')) # must be integer
 
 if now.year <= 2035: # if it's not 2035 yet, just subtract
 	years_to_go = 2035 - now.year
-	future_age = int(years_to_go) + int(current_age)
+	future_age = int(years_to_go) + current_age
 	print('You\'ll be {} in 2035.'.format(str(future_age)))
 elif now.year > 2035: # if it's past 2035, show error
 	print('It\'s past 2035! I don\'t feel like calculating your age anymore. Bye!')
