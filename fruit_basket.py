0def9e0b1a7cfd825763fe6055e06994933fa8c9
Fang <code@getbraintree.com>
2018-02-20 20:47:49 -0600

    added functions

--- fruit_basket.py
+++ fruit_basket.py
@@ -2,14 +2,27 @@ import sys
 
 fruit_basket = ['apple', 'mango', 'persimmon', 'blackberry', 'rambutan']
 
-guess = input('Guess a fruit in my basket. Singular form, please! ')
+# invoke the program with main()
 
-for fruit in fruit_basket:
-	if guess == fruit: 
-		print('You guessed right! I have that fruit. Good job!')
-		break
-	else:
-		continue
+def main():
+    guess_count = 0
+    while guess_count < 5:
+        guess = guess_fruit(fruit_basket)
+        
+        if guess:
+            print("Good job!")
+            return
+        elif not guess:
+            print("I don't have that fruit.")
+            guess_count += 1
+        else:
+            print("You're out of guesses.")
+            return
 
-print('Sorry, try again.')
-	
+# create a function called guess_fruit()
+
+def guess_fruit(basket):
+    guess = input("Guess a fruit in my basket: ")
+    return (guess in basket)
+
+main()
